a5cf789bf3270e88ba510eb3d39da130afb0d411
raja <abc@gmail.com>
2023-04-27 12:22:20 +0530

    presentation commit

---
+++ Project1/Practise/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Practise
+{
+
+    public class Class1
+    {
+        const String name = "Raja";
+        const int id = 1;
+
+       // id =100;
+
+            public static void Main(String[] args)
+        {
+         //   Class1.name = "ASDAS";
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ Project1/Practise/Sealed.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Practise
+{
+     public sealed class MainEx2
+    {
+        String name = "Abishek";
+
+        public void over()
+        {
+            Console.WriteLine(name);
+        }
+
+        
+        public static void Main(String[] args)
+        {
+            MainEx2 main = new MainEx2();
+            main.name = "asds";
+            main.over();
+
+        }
+
+    }
+
+   
+}
